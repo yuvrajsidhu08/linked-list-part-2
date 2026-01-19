30829a8c7754cb84b40a4320ce9ddce2755b5ec6
yuvrajsidhu08 <454009@bsd48.org>
2026-01-18 21:08:32 -0800

    commit

--- main.cpp
+++ main.cpp
@@ -13,3 +13,27 @@ Node* add(Node* head, Student* s) {
   head->set_next(add(head->get_next(), s));
   return head;
 }
+
+void print(Node* head) {
+  if (!head) return;
+  Student* s = head->get_value();
+  cout << s->getFirst() << " " << s->getLast()
+       << ", " << s->getID()
+       << ", " << fixed << setprecision(2)
+       << s->getGPA() << endl;
+  print(head->get_next());
+}
+
+Node* remove(Node* head, int id) {
+  if (!head) return NULL;
+
+  if (head->get_value()->getID() == id) {
+    Node* next = head->get_next();
+    delete head->get_value();
+    delete head;
+    return next;
+  }
+
+  head->set_next(remove(head->get_next(), id));
+  return head;
+}
