0f31460c95b8fa630bc9696272892826ccf02ff4
yuvrajsidhu08 <454009@bsd48.org>
2026-01-18 19:43:08 -0800

    commit

--- student.h
+++ student.h
@@ -6,4 +6,16 @@ class Student {
     Student(const char*, const char*, int, float);
     ~Student();
 
+    const char* getFirst();
+    const char* getLast();
+    int getID();
+    float getGPA();
+
+  private:
+    char* first;
+    char* last;
+    int id;
+    float gpa;
 };
+
+#endif
