fe84338f432c8e87efbff71b294db2595eb9f123
yuvrajsidhu08 <454009@bsd48.org>
2026-01-18 18:44:10 -0800

    commit

--- student.cpp
+++ student.cpp
@@ -4,4 +4,20 @@
 Student::Student(const char* f, const char* l, int i, float g) {
   first = new char[strlen(f) + 1];
   strcpy(first, f);
+
+  last = new char[strlen(l) + 1];
+  strcpy(last, l);
+
+  id = i;
+  gpa = g;
 }
+
+Student::~Student() {
+  delete[] first;
+  delete[] last;
+}
+
+const char* Student::getFirst() { return first; }
+const char* Student::getLast() { return last; }
+int Student::getID() { return id; }
+float Student::getGPA() { return gpa; }
